a2dd3a9b425e48db1454a3dfb6d5b2dba720054d
Ianis <ianis.bordrez@gmail.com>
2020-02-28 17:40:12 +0100

    version1

---
+++ controle1/script.sh
@@ -0,0 +1,19 @@
+PATH_IN=/tmp/in
+PATH_OUT=/tmp/out
+
+if [ ! -d $PATH_IN ]; then
+    echo "Il manque le dossier /tmp/in"
+    exit 3
+elif [ ! "$(ls -A $PATH_IN)" ]; then
+    echo "Pas de fichier a compresser"
+    exit 4
+elif  [ ! -d $PATH_OUT ]; then
+    mkdir $PATH_OUT
+fi
+
+for f in $PATH_IN/*
+do
+    echo "Processing $f file.."
+    gzip $f
+    mv $f.gz $PATH_OUT
+done
