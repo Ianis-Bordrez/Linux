a4737b6905f00e51414508578db29247d21d747d
Ianis <ianis.bordrez@gmail.com>
2020-02-28 18:07:43 +0100

    version3

--- controle1/script.sh
+++ controle1/script.sh
@@ -1,15 +1,16 @@
 PATH_IN=/tmp/in
 PATH_OUT=/tmp/out
+EXIT_ERROR=0
 
-if [ ! -d $PATH_IN ]; then
+if [ ! -d $PATH_IN ] then
     echo "Il manque le dossier /tmp/in"
     exit 3
-elif [ ! "$(ls -A $PATH_IN)" ]; then
+elif [ ! "$(ls -A $PATH_IN)" ] then
     echo "Pas de fichier a compresser"
     exit 4
-elif [ ! -d $PATH_OUT ]; then
+elif [ ! -d $PATH_OUT ] then
     mkdir $PATH_OUT
-elif [ -f $PATH_OUT/lock ]; then
+elif [ -f $PATH_OUT/lock ] then
     echo "Le script est deja en cours d'execution"
     exit 22
 fi
@@ -18,9 +19,16 @@ touch $PATH_OUT/lock
 
 for f in $PATH_IN/*
 do
+{
     echo "Compression de $f.."
     gzip $f
     mv $f.gz $PATH_OUT
+} || {
+    echo "Une erreur est survenue sur le fichier $f"
+    EXIT_ERROR=10
+}
+    
 done
-
 rm $PATH_OUT/lock
+
+exit $EXIT_ERROR
